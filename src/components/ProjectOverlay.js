2f44459cc5d5c0369786040e8b8b788f7afad881
djordjijem <djordjijem@gmail.com>
2019-09-05 00:46:18 +0200

    finish TaskDate and ProjectOverlay

--- src/components/ProjectOverlay.js
+++ src/components/ProjectOverlay.js
@@ -1,7 +1,47 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { useProjectsValue } from '../context';
 
-function ProjectOverlay() {
-  return <div>project overlay</div>;
+function ProjectOverlay({
+  setProject,
+  showProjectOverlay,
+  setShowProjectOverlay
+}) {
+  const { projects } = useProjectsValue();
+
+  return (
+    projects &&
+    showProjectOverlay && (
+      <div className='project-overlay' data-testid='project-overlay'>
+        <ul className='project-overlay__list'>
+          {projects.map(project => (
+            <li key={project.projectId}>
+              <div
+                data-testid='project-overlay-action'
+                onClick={() => {
+                  setProject(project.projectId);
+                  setShowProjectOverlay(false);
+                }}
+                onKeyDown={() => {
+                  setProject(project.projectId);
+                  setShowProjectOverlay(false);
+                }}
+                role='button'
+                tabIndex={0}
+                aria-label='Select the task project'
+              >
+                {project.name}
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    )
+  );
 }
+
+ProjectOverlay.propTypes = {
+  projects: PropTypes.array
+};
 
 export { ProjectOverlay };
